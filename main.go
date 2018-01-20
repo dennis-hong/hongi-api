c740fec64c4cfcfc45e6bb7fe182287eb8016183
dennis.hong <dennis.hong@kakaocorp.com>
2018-01-20 14:14:47 +0900

    test2

--- main.go
+++ main.go
@@ -15,22 +15,26 @@ type Action struct {
 
 func main() {
 	r := gin.Default()
-	r.Use(gin.Logger())
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.POST("/somePost", func(c *gin.Context) {
-		var kakaoReq KakaoReq
-		c.BindJSON(kakaoReq)
-		keys := make([]string, 0, len(kakaoReq.UserAction.Params))
-		for k := range kakaoReq.UserAction.Params {
-			keys = append(keys, k)
-			log.Println(keys)
-		}
 
-		c.JSON(200, gin.H{"user": "", "value": ""})
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+		v1.POST("/somePost", func(c *gin.Context) {
+			var kakaoReq KakaoReq
+			c.BindJSON(&kakaoReq)
+			keys := make([]string, 0, len(kakaoReq.UserAction.Params))
+			for k := range kakaoReq.UserAction.Params {
+				keys = append(keys, k)
+				log.Println(keys)
+			}
+
+			c.JSON(200, gin.H{"user": "", "value": ""})
+		})
+	}
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
